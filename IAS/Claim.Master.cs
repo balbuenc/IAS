d68f3976f12315a341a288a54a6b3e76d2bc09f8
Ricardo Rodriguez <ri_rodrig@outlook.com>
2016-01-21 13:06:13 -0300

    Autenticación de la master de Siniestros

--- IAS/Claim.Master.cs
+++ IAS/Claim.Master.cs
@@ -13,36 +13,42 @@ namespace IAS
         protected void Page_Load(object sender, EventArgs e)
         {
 
-            string filename = Path.GetFileName(Request.Path);
+            if (!IsPostBack && Request.Url.LocalPath.ToString() != "/Account/Login")
+            {
+                Core.Core User = new Core.Core();
 
-            //Limpio los campos de Atributos de las pastillas de Navegacion
-            searchClaimTab.Attributes.Remove("class");
-            newClaimTab.Attributes.Remove("class");
-            verificationClaimTab.Attributes.Remove("class");
-            registerClaimTab.Attributes.Remove("class");
-            approvementClaimTab.Attributes.Remove("class");
-            workShopClaimTab.Attributes.Remove("class");
-            closeClaimTab.Attributes.Remove("class");
+                if (!User.IsUserAuthenticate())
+                {
+                    Response.Redirect("/Account/Login.aspx", false);
+                }
 
-            //Seteo la pastilla de Navegacion activa
-            if (filename == "ClaimSearch")
-                searchClaimTab.Attributes.Add("class","active");
-            else if (filename == "NewClaim")
-                newClaimTab.Attributes.Add("class", "active");
-            else if (filename == "ClaimPolicyVerification")
-                verificationClaimTab.Attributes.Add("class", "active");
-            else if (filename == "ClaimRegistration")
-                registerClaimTab.Attributes.Add("class", "active");
-            else if (filename == "CloseClaim")
-                closeClaimTab.Attributes.Add("class", "active");
-            else if (filename == "ApprovementClaim")
-                approvementClaimTab.Attributes.Add("class", "active");
-            else if (filename == "WorkShopClaim")
-                workShopClaimTab.Attributes.Add("class", "active");
+                string filename = Path.GetFileName(Request.Path);
 
+                //Limpio los campos de Atributos de las pastillas de Navegacion
+                searchClaimTab.Attributes.Remove("class");
+                newClaimTab.Attributes.Remove("class");
+                verificationClaimTab.Attributes.Remove("class");
+                registerClaimTab.Attributes.Remove("class");
+                approvementClaimTab.Attributes.Remove("class");
+                workShopClaimTab.Attributes.Remove("class");
+                closeClaimTab.Attributes.Remove("class");
+
+                //Seteo la pastilla de Navegacion activa
+                if (filename == "ClaimSearch")
+                    searchClaimTab.Attributes.Add("class", "active");
+                else if (filename == "NewClaim")
+                    newClaimTab.Attributes.Add("class", "active");
+                else if (filename == "ClaimPolicyVerification")
+                    verificationClaimTab.Attributes.Add("class", "active");
+                else if (filename == "ClaimRegistration")
+                    registerClaimTab.Attributes.Add("class", "active");
+                else if (filename == "CloseClaim")
+                    closeClaimTab.Attributes.Add("class", "active");
+                else if (filename == "ApprovementClaim")
+                    approvementClaimTab.Attributes.Add("class", "active");
+                else if (filename == "WorkShopClaim")
+                    workShopClaimTab.Attributes.Add("class", "active");
+            }
         }
-
-
-
     }
 }

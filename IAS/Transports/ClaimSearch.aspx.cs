8cdd02edab5d9b64bcdd8ef2cd78106658bbe3aa
Ricardo Rodriguez <ri_rodrig@outlook.com>
2016-10-12 10:46:19 -0300

    Insert de Transport.Claim

---
+++ IAS/Transports/ClaimSearch.aspx.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace IAS.Transports
+{
+    public partial class ClaimSearch : Page
+    {
+        private string criteria;
+        private string claimID;
+
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            ClaimSqldataSource.SelectParameters["user"].DefaultValue = User.Identity.Name;
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                //Verifico si es una busqueda por Formulario
+                try
+                {
+
+                    if (Request.QueryString["PolicyNumber"] != null)
+                    {
+                        claimID = Request.QueryString["PolicyNumber"].ToString();
+                    }
+
+                    if (claimID != null)
+                    {
+                        txtSearchClaim.Text = claimID;
+                        ClaimListView.DataSource = null;
+                        ClaimListView.DataSourceID = "ClaimSqldataSource";
+                        ClaimListView.DataBind();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    claimID = null;
+                }
+
+                try
+                {
+                    if (Request.QueryString["criteria"] != null)
+                    {
+                        criteria = Request.QueryString["criteria"].ToString();
+                        switch (criteria)
+                        {
+                            case "PolicyNumber":
+                                txtSearchClaim.Attributes["placeholder"] = "Buscar por Nro. Póliza";
+                                //criteriaBtn.InnerText = "Nro. Póliza";
+                                break;
+                            case "Client":
+                                txtSearchClaim.Attributes["placeholder"] = "Buscar por Cliente";
+                                //criteriaBtn.InnerText = "Cliente";
+                                break;
+                            case "ClientDocumentNumber":
+                                txtSearchClaim.Attributes["placeholder"] = "Buscar por Nro. Documento";
+                                //criteriaBtn.InnerText = "Nro. Documento";
+                                break;
+                            case "ClaimNumber":
+                                txtSearchClaim.Attributes["placeholder"] = "Buscar por Nro. Siniestro";
+                                //criteriaBtn.InnerText = "Nro. Siniestro";
+                                break;
+                            default:
+                                txtSearchClaim.Attributes["placeholder"] = "Buscar por Nro. Pòliza";
+                                //criteriaBtn.InnerText = "Nro. Póliza";
+                                criteria = "PolicyNumber";
+                                break;
+                        }
+                    }
+
+                }
+                catch (Exception exp)
+                {
+                    txtSearchClaim.Attributes["placeholder"] = "Buscar por Nro. Póliza";
+                    //criteriaBtn.InnerText = "Nro. Póliza";
+                    criteria = "PolicyNumber";
+                }
+            }
+        }
+
+        protected void searchClaims()
+        {
+            string criteria,
+                   find,
+                   myClaims,
+                   claimStatusId;
+
+            DateTime? startDate = null,
+                     endDate = null;
+
+            if (!string.IsNullOrEmpty(dpStart.Value))
+            {
+                startDate = DateTime.Parse(dpStart.Value);
+            }
+
+            if (!string.IsNullOrEmpty(dpEnd.Value))
+            {
+                endDate = DateTime.Parse(dpEnd.Value);
+            }
+
+            if (txtSearchClaim.Text == string.Empty)
+                find = " ";
+            else
+            {
+                find = txtSearchClaim.Text;
+            }
+
+            criteria = "Client";//ddlCriteria.SelectedValue;
+            myClaims = ddlMyClaims.SelectedValue;
+            claimStatusId = ddlStatus.SelectedValue;
+
+            SqlConnection sqlConnection1 = new SqlConnection(ClaimSqldataSource.ConnectionString);
+            SqlDataAdapter da = new SqlDataAdapter();
+            SqlCommand cmd = new SqlCommand();
+            DataTable dt = new DataTable();
+
+            try
+            {
+                cmd.CommandText = "[claim].[sp_search_claims]";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Connection = sqlConnection1;
+
+                cmd.Parameters.AddWithValue("@find", find);
+                cmd.Parameters.AddWithValue("@criteria", criteria);
+                cmd.Parameters.AddWithValue("@user", User.Identity.Name);
+                cmd.Parameters.AddWithValue("@myClaims", myClaims);
+                cmd.Parameters.AddWithValue("@claimStatusId", claimStatusId);
+
+                cmd.Parameters.Add("@startDate", SqlDbType.DateTime).Value = startDate;
+                cmd.Parameters.Add("@endDate", SqlDbType.DateTime).Value = endDate;
+
+                da.SelectCommand = cmd;
+
+                da.Fill(dt);
+
+                //if (dt != null && dt.Rows.Count > 0)
+                //{
+                ClaimListView.DataSourceID = string.Empty;
+                ClaimListView.DataSource = dt;
+                ClaimListView.DataBind();
+                //}
+            }
+            catch (Exception exp)
+            {
+                ErrorLabel.Text = "Error a ejecutar busqueda.. : " + exp.Message;
+                ErrorLabel.Visible = true;
+            }
+        }
+
+        protected void ClaimListView_ItemCommand(object sender, ListViewCommandEventArgs e)
+        {
+            SqlConnection sqlConnection1 = new SqlConnection(ClaimSqldataSource.ConnectionString);
+            SqlCommand cmd = new SqlCommand();
+
+            int rowsAffected;
+
+            string claimID = ((Label)e.Item.FindControl("lblClaimID")).Text;
+            string PolicyNumber = ((Label)e.Item.FindControl("lblPolicyNumber")).Text;
+
+            if (e.CommandName == "Edit")
+            {
+                Response.Redirect("ClaimDetail.aspx?ClaimID=" + claimID + "&PolicyNumber=" + PolicyNumber);
+            }
+            else if (e.CommandName == "Close")
+            {
+                try
+                {
+
+                    //Genero el cambio de estado
+                    cmd.CommandText = "claim.sp_change_claim_status";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Connection = sqlConnection1;
+
+                    cmd.Parameters.AddWithValue("@ClaimID", claimID);
+                    cmd.Parameters.AddWithValue("@GoNextStep", -1);
+                    cmd.Parameters.AddWithValue("@UserName", User.Identity.Name);
+
+                    sqlConnection1.Open();
+
+                    rowsAffected = cmd.ExecuteNonQuery();
+
+                    sqlConnection1.Close();
+
+
+
+                    //Direcciono a la pagina de busqueda
+                    Response.Redirect("ClaimSearch.aspx?PolicyNumber=" + PolicyNumber);
+
+                }
+                catch (Exception exp)
+                {
+                    ErrorLabel.Text = exp.Message;
+                    ErrorLabel.Visible = true;
+                }
+            }
+
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            searchClaims();
+        }
+
+        [WebMethod]
+        public static string[] SearchClaim(string prefix)
+        {
+            List<string> customers = new List<string>();
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = @"Data Source = aibsql.cloudapp.net,1500; Initial Catalog = ias_developer; Persist Security Info = True; User ID = ias_desarrollo; Password = Passw0rd";
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.CommandText = "select Top 10 nombre, apellido1 from exchange.personas where nombre like ''+@SearchText+'%'";
+                    cmd.Parameters.AddWithValue("@SearchText", prefix);
+                    cmd.Connection = conn;
+                    conn.Open();
+                    using (SqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        while (sdr.Read())
+                        {
+                            customers.Add(string.Format("{0}-{1}", sdr["nombre"], sdr["apellido1"]));
+                        }
+                    }
+                    conn.Close();
+                }
+            }
+            return customers.ToArray();
+        }
+
+    }
+}
---
+++ IAS/Transports/NewClaim.aspx.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace IAS.Transports
+{
+    public partial class NewClaim : System.Web.UI.Page
+    {
+        public string nro_poliza
+        {
+            get
+            {
+                object o = ViewState["nro_poliza"];
+                if (o == null)
+                    return string.Empty;
+                return o.ToString();
+            }
+            set
+            {
+                ViewState["nro_poliza"] = value;
+            }
+        }
+
+        public long certificateID
+        {
+            get
+            {
+                object o = ViewState["certificateID"];
+                if (o == null)
+                    return 0;
+                return (long)o;
+            }
+            set
+            {
+                ViewState["certificateID"] = value;
+            }
+        }
+
+        public string id_persona
+        {
+            get
+            {
+                object o = ViewState["id_persona"];
+                if (o == null)
+                    return string.Empty;
+                return o.ToString();
+            }
+            set
+            {
+                ViewState["id_persona"] = value;
+            }
+        }
+
+        private string criteria;
+        private string find;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                if (Request.QueryString["criteria"] != null)
+                {
+                    criteria = Request.QueryString["criteria"].ToString();
+                    switch (criteria)
+                    {
+                        case "PolicyNumber":
+                            txtSearchClaim.Attributes["placeholder"] = "Buscar por Nro. Póliza";
+                            criteriaBtn.InnerText = "Nro. Póliza";
+                            break;
+                        case "Client":
+                            txtSearchClaim.Attributes["placeholder"] = "Buscar por Cliente";
+                            criteriaBtn.InnerText = "Cliente";
+                            break;
+                        case "ClientDocumentNumber":
+                            txtSearchClaim.Attributes["placeholder"] = "Buscar por Nro. Documento";
+                            criteriaBtn.InnerText = "Nro. Documento";
+                            break;
+
+                        default:
+                            txtSearchClaim.Attributes["placeholder"] = "Buscar por Nro. Pòliza";
+                            criteriaBtn.InnerText = "Nro. Pòliza";
+                            criteria = "PolicyNumber";
+                            break;
+                    }
+                }
+                else
+                {
+                    criteria = "Client";
+                }
+
+            }
+            catch (Exception exp)
+            {
+                txtSearchClaim.Attributes["placeholder"] = "Buscar por Nro. Póliza";
+                criteriaBtn.InnerText = "Nro. Póliza";
+                criteria = "PolicyNumber";
+            }
+        }
+
+        protected void registrarSiniestroBtn_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlConnection1 = new SqlConnection(clientesDataSource.ConnectionString);
+            SqlCommand cmd = new SqlCommand();
+            int claimID;
+            int rowsAffected;
+
+            try
+            {
+
+                cmd.CommandText = "[transport].[sp_registrar_caso_siniestro]";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Connection = sqlConnection1;
+
+                SqlParameter claim_id = new SqlParameter("@id_claim", SqlDbType.Int)
+                {
+                    Direction = ParameterDirection.Output
+                };
+
+                cmd.Parameters.Add(claim_id);
+                cmd.Parameters.AddWithValue("@id_persona", id_persona);
+                cmd.Parameters.AddWithValue("@cliente", lblCliente.Text);
+                cmd.Parameters.AddWithValue("@fecha_siniestro", Convert.ToDateTime(dp1.Value));
+                cmd.Parameters.AddWithValue("@observacion", txtObservacion.Text);
+                cmd.Parameters.AddWithValue("@nro_poliza", nro_poliza);
+                cmd.Parameters.AddWithValue("@id_certificado", certificateID);
+                cmd.Parameters.AddWithValue("@usuario", User.Identity.Name);
+
+                sqlConnection1.Open();
+
+                rowsAffected = cmd.ExecuteNonQuery();
+                claimID = int.Parse(cmd.Parameters["@id_claim"].Value.ToString());
+
+                sqlConnection1.Close();
+
+                //SaveDetails(claimID);
+
+                //Direcciono a la pagina de busqueda
+                Response.Redirect("ClaimSearch.aspx?PolicyNumber=" + nro_poliza.Replace(".", ""));
+
+            }
+            catch (Exception exp)
+            {
+                ErrorLabel.Text = exp.Message;
+                ErrorLabel.Visible = true;
+            }
+        }
+
+        //private void SaveDetails(int claimID)
+        //{
+
+        //    SqlConnection sqlConnection1 = new SqlConnection(clientesDataSource.ConnectionString);
+        //    int rowsAffected;
+
+        //    foreach (ListItem item in lsbCoberturas.Items)
+        //    {
+        //        if (item.Selected)
+        //        {
+        //            using (SqlCommand cmd = new SqlCommand())
+        //            {
+
+        //                try
+        //                {
+
+        //                    cmd.CommandText = "sp_registrar_caso_siniestro_detalle";
+        //                    cmd.CommandType = CommandType.StoredProcedure;
+        //                    cmd.Connection = sqlConnection1;
+
+        //                    string val = item.Value;
+
+        //                    cmd.Parameters.AddWithValue("@id_claim", claimID);
+        //                    cmd.Parameters.AddWithValue("@id_cobertura", val);
+
+        //                    sqlConnection1.Open();
+
+        //                    rowsAffected = cmd.ExecuteNonQuery();
+
+        //                    sqlConnection1.Close();
+        //                }
+        //                catch (Exception exp)
+        //                {
+        //                    ErrorLabel.Text = exp.Message;
+        //                    ErrorLabel.Visible = true;
+        //                }
+        //            }
+        //        }
+        //    }
+
+        //}
+
+        protected void searchBox_ServerClick(object sender, EventArgs e)
+        {
+            SqlConnection sqlConnection1 = new SqlConnection(clientesDataSource.ConnectionString);
+            SqlCommand cmd = new SqlCommand();
+
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataTable dt = new DataTable();
+
+            try
+            {
+                cmd.CommandText = "[transport].[sp_search_clients_for_claims]";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Connection = sqlConnection1;
+
+                cmd.Parameters.AddWithValue("@find", txtSearchClaim.Value);
+                cmd.Parameters.AddWithValue("@criteria", criteria);
+
+                da.SelectCommand = cmd;
+
+                da.Fill(dt);
+
+                gridClients.DataSource = dt;
+                gridClients.DataBind();
+
+            }
+            catch (Exception exp)
+            {
+                ErrorLabel.Text = exp.Message;
+                ErrorLabel.Visible = true;
+            }
+
+            ScriptManager.RegisterStartupScript(this, GetType(), "Pop", "openModalPolizas();", true);
+        }
+
+        protected void gridClients_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+
+            switch (e.CommandName)
+            {
+                case "seleccionar":
+                    string[] values = e.CommandArgument.ToString().Split('|');
+
+                    string detail = values[0];
+                    string numero_documento = values[1];
+                    string cliente = values[2];
+                    string idPersona = values[3];
+                    string nroPoliza = values[4];
+                    string certificadoID = values[5];
+                    lblPoliza.Text = detail;
+                    lblNroDocumento.Text = numero_documento;
+                    lblCliente.Text = cliente;
+
+                    id_persona = idPersona;
+                    nro_poliza = nroPoliza;
+                    certificateID = int.Parse(certificadoID);
+                    break;
+                default:
+                    break;
+
+
+            }
+
+        }
+
+    }
+}

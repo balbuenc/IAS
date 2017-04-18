50cf443b1d1e44dd91eb0f89136c0343477e0a0c
Ricardo Rodriguez <ri_rodrig@outlook.com>
2017-04-18 14:03:00 -0400

    se agrego log4net al proyecto

--- IAS/Global.asax.cs
+++ IAS/Global.asax.cs
@@ -15,6 +15,7 @@ namespace IAS
     {
         void Application_Start(object sender, EventArgs e)
         {
+            Logger.InicializeLogging();
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
